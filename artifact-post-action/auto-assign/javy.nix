7f6baf99d104030e3d5ebe519579d8b6238ee3a1
Thomas Gerbet <thomas.gerbet@enalean.com>
2025-09-19 17:02:47 +0200

    chore: request #44844 Update dev/build environment [W38 2025]
    The main change is the Javy bump 4.0.0 -> 6.0.0:
    https://github.com/bytecodealliance/javy/releases/tag/v6.0.0
    https://github.com/bytecodealliance/javy/releases/tag/v5.0.0

--- artifact-post-action/auto-assign/javy.nix
+++ artifact-post-action/auto-assign/javy.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation rec {
   pname = "javy";
-  version = "4.0.0";
+  version = "6.0.0";
 
   src = fetchurl {
     url = "https://github.com/bytecodealliance/javy/releases/download/v${version}/javy-x86_64-linux-v${version}.gz";
-    hash = "sha256-sPVOFmDeQ4RAw35X6D4xAbV0jvZVtPiEe+XPYvJFUhQ=";
+    hash = "sha256-O2VdNxB5f+ywfEG17T+EcFwf0Ox9HthW/8WC/GPT3fE=";
   };
 
   nativeBuildInputs = [
